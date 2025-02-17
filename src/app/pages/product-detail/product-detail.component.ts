89a60d58615332f7fdd6ac4e84f0d7ec3d431de9
josee022 <jose.mondelo@iesdonana.org>
2025-02-17 20:51:12 +0100

    listo primeros pasos angular landing page

--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../model/product.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-detail.component.html',
-  styleUrl: './product-detail.component.css'
+  styleUrl: './product-detail.component.css',
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  loading: boolean = true;
+  public product?: IProduct;
 
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this._route.params.subscribe((params) => {
+      this._apiService.getProduct(params['id']).subscribe((data: IProduct) => {
+        this.product = data;
+        this.loading = false;
+      });
+    });
+  }
 }
