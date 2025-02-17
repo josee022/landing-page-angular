115d1a2197181cf007740fc0cc099420ebf6b5dc
josee022 <jose.mondelo@iesdonana.org>
2025-02-17 19:09:02 +0100

    primeras interacciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'landingPage';
+
+  menuOption = '';
+
+  onOption(menuOption: string) {
+    this.menuOption = menuOption;
+  }
+
 }
