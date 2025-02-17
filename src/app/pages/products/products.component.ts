40ebf28139615efea4336844584d9fbd39eb5393
josee022 <jose.mondelo@iesdonana.org>
2025-02-17 19:50:57 +0100

    products

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../model/product.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  productList: IProduct[] = [];
+  private _apiService = inject(ApiService);
+  private _router = inject(Router);
 
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe((data: IProduct[]) => {
+      this.productList = data;
+    });
+  }
+
+  navegate(id: number): void {
+    this._router.navigate(['/products', id]);
+  }
+  
 }
